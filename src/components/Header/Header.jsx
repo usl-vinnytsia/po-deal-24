eacda564756cea2987151959a3464d02932397b1
alinavovkov <alinvovkov@gmail.com>
2024-10-22 15:43:51 +0300

    added header basic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-scroll';
 import './Header.scss';
 
+import logo from '../../images/DealLogo.svg';
+import arrow from '../../images/arrow.png'
+
 const Header = () => {
   return (
     <header className="header">
+      <img src={logo} alt="" srcset="" class="logo"></img>
       <nav>
         <ul>
-          <li><Link to="main-screen" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-          <li><Link to="team" smooth={true} duration={500}>Team</Link></li>
+          <li><Link to="main-screen" smooth={true} duration={500}>про нас</Link></li>
+          <li><Link to="about" smooth={true} duration={500}>партнери</Link></li>
+          <li><Link to="team" smooth={true} duration={500}>програма</Link></li>
         </ul>
       </nav>
+      <div>
+        <a href="http://">реєстрація</a>
+        <img src={arrow} alt="arrow" class="header-arrow"></img>
+      </div>
     </header>
   );
 };
