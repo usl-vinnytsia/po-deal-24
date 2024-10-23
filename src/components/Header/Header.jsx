8c26d43a5f27a92dee773364c641299c9b992a82
2pasha <kostishinpavlo24@gmail.com>
2024-10-23 13:14:59 +0300

    base logic of position header was implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,17 +7,19 @@ import arrow from '../../images/arrow.png'
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} alt="" srcset="" class="logo"></img>
-      <nav>
-        <ul>
-          <li><Link to="main-screen" smooth={true} duration={500}>про нас</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>партнери</Link></li>
-          <li><Link to="team" smooth={true} duration={500}>програма</Link></li>
-        </ul>
-      </nav>
-      <div>
-        <a href="http://">реєстрація</a>
-        <img src={arrow} alt="arrow" class="header-arrow"></img>
+      <div className="container">
+        <img src={logo} alt="" className="logo"></img>
+        <nav>
+          <ul>
+            <li><Link to="main-screen" smooth={true} duration={500}>про нас</Link></li>
+            <li><Link to="about" smooth={true} duration={500}>партнери</Link></li>
+            <li><Link to="team" smooth={true} duration={500}>програма</Link></li>
+          </ul>
+        </nav>
+        <div>
+          <a href="http://">реєстрація</a>
+          <img src={arrow} alt="arrow" className="header-arrow"></img>
+        </div>
       </div>
     </header>
   );
