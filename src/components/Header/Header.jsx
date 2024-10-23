edab16fc823a7cc8d6b43d4934667f8b099e0b98
2pasha <kostishinpavlo24@gmail.com>
2024-10-23 14:12:31 +0300

    header was done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,30 @@ import { Link } from 'react-scroll';
 import './Header.scss';
 
 import logo from '../../images/DealLogo.svg';
-import arrow from '../../images/arrow.png'
+import arrow from '../../images/arrow.svg'
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
-        <img src={logo} alt="" className="logo"></img>
+      <div className="header__container">
+        <img src={logo} alt="" className="header__logo"></img>
         <nav>
           <ul>
-            <li><Link to="main-screen" smooth={true} duration={500}>про нас</Link></li>
-            <li><Link to="about" smooth={true} duration={500}>партнери</Link></li>
-            <li><Link to="team" smooth={true} duration={500}>програма</Link></li>
+            <li>
+              <Link to="main-screen" smooth={true} duration={500}>про нас</Link>
+            </li>
+            <li>
+              <Link to="about" smooth={true} duration={500}>партнери</Link>
+            </li>
+            <li>
+              <Link to="team" smooth={true} duration={500}>програма</Link>
+            </li>
           </ul>
         </nav>
-        <div>
-          <a href="http://">реєстрація</a>
-          <img src={arrow} alt="arrow" className="header-arrow"></img>
-        </div>
+        <a href="https://t.me/PoDeal_bot" className="header__register">
+          <span>реєстрація</span>
+          <img src={arrow} alt="arrow"></img>
+        </a>
       </div>
     </header>
   );
