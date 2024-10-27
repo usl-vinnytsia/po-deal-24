580a070a54d7e02e776770517a0b9b1c15641146
alinavovkov <alinvovkov@gmail.com>
2024-10-27 13:23:32 +0200

    the page of team was remade, the page of program was done and changes in the top menu (the correct links)

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -3,8 +3,45 @@ import './Program.scss';
 export const Program = () => {
   return (
     <section id="program" className="program">
-      <h2>Program</h2>
-      <p>Here is Program screen.</p>
+      <main className='program-main'>
+        <div className='program-main-about'>
+          <span>[ярмарок карʼєри]</span>
+          <span>ярмарок карʼєри / 20 локацій</span>
+          <span>10 спікерів</span>
+          <span className='program-main-title'>програма подолу</span>
+        </div>
+
+        <div className='program-main-bottom-txt'>
+          <p>Time felt suspended, as if the world itself held its breath, waiting for something to </p>
+          <span>©2024-2025</span>
+        </div>
+      </main>
+      <div className='program__container'>
+        <table>
+          <tr>
+            <td>[9.00-10.00]</td>
+            <td>як відмовляти людям</td>
+            <td>Михайло Ганчук</td>
+          </tr>
+          <tr>
+            <td>[9.00-10.00]</td>
+            <td>чи може Росія розпочати війну проти України</td>
+            <td>Володимир Зеленський, Гройсан, Стів Джобс, Святослав Вакарчук</td>
+          </tr>
+          <tr>
+            <td>[all day]</td>
+            <td>консультативний HR центр</td>
+            <td>Володимир Зеленський, Гройсан, Стів Джобс, Святослав Вакарчук</td>
+          </tr>
+          <tr>
+            <td>[9.00-10.00]</td>
+            <td>як відмовляти людям</td>
+            <td>Михайло Ганчук</td>
+          </tr>
+        </table>
+
+      </div>
+
     </section>
   );
 };
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,10 +1,33 @@
 import './Team.scss';
+import teamLeft from '../../images/teamLeft.png';
+import teamRight from '../../images/teamRight.png';
+
 
 const Team = () => {
   return (
     <section id="team" className="team">
-      <h2>Team</h2>
-      <p>Here is about team.</p>
+      <div className=' team__container'>
+        <aside className='team-left'>
+          <img src={teamLeft} alt="" srcset="" />
+          <div className='team-left-photo-descr'>
+            <span>УСЛ ВП</span>
+            <span>Вінниця</span>
+          </div>
+          <p className='team-left-descr'>
+            Beneath the silver-hued sky, the crimson sea stretched endlessly, reflecting the gentle glow of distant stars. A lone ship drifted quietly, its sails catching whispers of wind that carried secrets from forgotten lands.
+            <br></br>
+            <br></br>
+            On the deck, a single lantern flickered, casting long shadows that danced to the rhythm of the ocean.
+          </p>
+        </aside>
+        <aside className='team-right'>
+          <img src={teamRight} alt="" className='team-right-img' />
+          <span className='team-right-title'>знайомимось з командою</span>
+
+          <p className='team-right-descr'>omewhere below, in the heart of the ship, an ancient compass began to spin wildly, as if drawn</p>
+        </aside>
+      </div>
+
     </section>
   );
 };
