efef9e40358716bb27065301e631f0fb9dab4b6c
alinavovkov <alinvovkov@gmail.com>
2024-10-28 17:59:18 +0200

    fulfilled the footer with relevant information

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import './Footer.scss';
 import arrow from '../../images/arrow.svg'
+import { Link } from 'react-scroll';
 
 export const Footer = () => {
   return (
@@ -8,7 +9,7 @@ export const Footer = () => {
         <span className='footer-mainTxt'>ми переконали</span>
         <div className='footer-sub-mainTxt'>
           <span className='footer-mainTxt'>тебе?</span>
-          <a href="https://t.me/PoDeal_bot" className="footer__register">
+          <a href="https://t.me/PoDeal_bot" className="footer__register" target='blank'>
             <span>реєстрація</span>
             <img src={arrow} alt="arrow"></img>
           </a>
@@ -20,43 +21,48 @@ export const Footer = () => {
               <a href="http://">Соціальні мережі</a>
             </li>
             <li>
-              <a href="http://">
-                Instagram
+              <a href="https://www.instagram.com/usl.vn/" target='blank'>
+                Вінницький відокремлений підрозділ УСЛ
+
               </a>
             </li>
             <li>
-              <a href="http://">Telegram
-              </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="http://">
-                про нас
-              </a>
-            </li>
-            <li>
-              <a href="http://">
-                Програма
-              </a>
-            </li>
-            <li>
-              <a href="http://">
-                Партнери
+              <a href="https://www.instagram.com/usl.org.ua/" target='blank'>
+                Українська студентьска ліга
               </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a href="http://">Соціальні мережі</a>
+              <Link to="team" smooth={true} duration={500}>про нас</Link>
             </li>
             <li>
-              <a href="http://">
-                Instagram
+              <Link to="program" smooth={true} duration={500}>програма</Link>
+            </li>
+            <li>
+              <Link to="partners" smooth={true} duration={500}>партнери</Link>
+            </li>
+            <li>
+              <Link to="partners" smooth={true} duration={500}>про ПоDeal `24</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Розроблено
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/dasha-dusaniuk/" target='blank'>
+                Дар'я (designer) 
               </a>
             </li>
             <li>
-              <a href="http://">Telegram
+              <a href="https://www.linkedin.com/in/alinavovkov/" target='blank'>
+                Аліна (developer)
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/pavlo-kostyshyn-5871b8196/" target='blank'>
+                Павло (developer)
               </a>
             </li>
           </ul>
