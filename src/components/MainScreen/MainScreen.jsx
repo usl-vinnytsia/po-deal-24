af62ad9528adea1b79c2d8c4da92fdc3093b08a4
2pasha <kostishinpavlo24@gmail.com>
2024-10-29 08:05:08 +0200

    main screen was adapted for mobile view

--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,5 +1,6 @@
 import './MainScreen.scss';
 import photo from '../../images/main-screen.jpg';
+import logo from '../../images/DealLogo.svg';
 
 const MainScreen = () => {
   return (
@@ -12,9 +13,9 @@ const MainScreen = () => {
             <span>твоя угода</span>
             <span className="title__bracket title__bracket--left">(</span>
             <div className="title__image">
-              <img 
-                src={photo} 
-                alt='main screen' 
+              <img
+                src={photo}
+                alt='main screen'
                 className='title__image--img' />
 
             </div>
@@ -23,10 +24,30 @@ const MainScreen = () => {
           </div>
         </div>
 
+        <div className="main-screen__title--mobile">
+          <span className="title__first-line">твоя угода</span>
+          <div className="title__second-line--mobile">
+            <span>на</span>
+            <img src={logo} alt='' className='main-screen__title--mobile--img' />
+          </div>
+
+          <div className="title__bracket-block">
+            <span className="title__bracket title__bracket--left">(</span>
+            <div className="title__image">
+              <img
+                src={photo}
+                alt='main screen'
+                className='title__image--img' />
+
+            </div>
+            <span className="title__bracket title__bracket--right">)</span>
+          </div>
+        </div>
+
         <div className="main-screen__info">
-          <p>благодійний карʼєрний ярмарок</p>
-          <p>Створений для об’єднання молодих талантів з бізнесом <br />Та надання можливості знайти своє місце в професійному світі, <br /> отримуючи цінні ресурси від провідних компаній</p>
-          <p>для студентів</p>
+          <p className='main-screen__info--left'>благодійний карʼєрний ярмарок</p>
+          <p className='main-screen__info--center'>Створений для об’єднання молодих талантів з бізнесом Та надання можливості знайти своє місце в професійному світі, отримуючи цінні ресурси від провідних компаній</p>
+          <p className='main-screen__info--right'>для студентів</p>
         </div>
       </div>
     </section>
